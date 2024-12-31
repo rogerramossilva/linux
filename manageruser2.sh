edf02e8ed6f9c93e64fa425f59817c638ea63e5e
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2024-12-30 22:16:42 -0300

    Create manageruser2.sh

---
+++ manageruser2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+. /var/scripts/funcoes.sh
+
+menu
+
+if [ $OPCAO -eq 1 ]; then
+  nome
+  validaruser
+  if [ $RC -eq 0 ]; then
+    echo "usuario $NOME já existe"
+  else
+    criaruser
+  fi  
+elif [ $OPCAO -eq 2 ]; then
+  nome
+  validauser
+  if [ $RC -eq 0 ]; then
+    deleteuser
+  else
+    echo "O Usuário $NOME não existe"
+  fi  
+elif [ $OPCAO -eq 3 ]; then
+nome
+  validauser
+  if [ $RC -eq 0 ]; then
+    alterasenha
+  else
+    echo "O Usuário $NOME Não existe"
+  fi
+else
+  echo "Opção invalida, Digite novamente"
+fi
